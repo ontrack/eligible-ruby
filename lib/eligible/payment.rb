fdc9aa834b5b27e053834b179cabe35fd3cc9993
Ralph <161543@szrzimmermann-m-2.local>
2014-10-30 08:00:14 -0700

    Changed payment status call to utilize new v1.4 url scheme

--- lib/eligible/payment.rb
+++ lib/eligible/payment.rb
@@ -3,8 +3,9 @@ module Eligible
 
     class << self
 
-      def get(params, api_key=nil)
-        response, api_key = Eligible.request(:get, "/payment/status.json", api_key, params)
+      def get(params, api_key=nil)        
+        puts "/claims/#{params[:reference_id]}/payment_status.json"
+        response, api_key = Eligible.request(:get, "/claims/#{params[:reference_id]}/payment_status.json", api_key, params)
         Util.convert_to_eligible_object(response, api_key)
       end
 
